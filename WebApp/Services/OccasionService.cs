a38ab69b54f55332ad544faa1587436d8f4153f2
ParkerS00 <parkerswenson00@gmail.com>
2024-03-13 11:11:30 -0600

    build 3

--- WebApp/Services/OccasionService.cs
+++ WebApp/Services/OccasionService.cs
@@ -19,7 +19,7 @@ namespace WebApp.Services
             var context = contextFactory.CreateDbContext();
             context.Add(occasion);
             context.SaveChangesAsync();
-            
+            //
         }
 
         public Task DropTables()
